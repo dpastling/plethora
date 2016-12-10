63e442824f3a744f1aa3ef07b44ca0df1f6103d3
David Astling <david.astling@ucdenver.edu>
2016-12-10 00:16:46 -0700

    refactor the batch_make bed script to use new system

--- code/batch_make_bed.sh
+++ code/batch_make_bed.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#BSUB -J coverage[1-5]%5
+#BSUB -J coverage[1-300]%5
 #BSUB -e logs/coverage_%J.log
 #BSUB -o logs/coverage_%J.out
 #BSUB -R "select[mem>40] rusage[mem=40]"
@@ -17,23 +17,5 @@ sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
 bam_file=$alignment_dir/${sample}
 bed_file=$bed_dir/$sample
 
-## Convert alignment file to a bed format
-samtools sort -n -@ 5 -m 5G $bam_file.bam ${bam_file}_sorted
-bedtools bamtobed -bedpe -split -i ${bam_file}_sorted.bam > $bed_file.bed
-
-# remove alignment files if bed has correct number of reads
-code/clean_files.pl -a $alignment_dir -b $bed_dir --rm_fastq $sample
-
-# Merge reads into fragments
-code/parse_bed.pl $bed_file.bed
-sort -k 1,1 -k 2,2n -T $bed_dir/ ${bed_file}_edited.bed > ${bed_file}_sorted.bed
-
-# Calcualte coverage
-bedtools intersect -wao -sorted -a $master_ref -b ${bed_file}_sorted.bed > ${bed_file}_temp.bed
-awk 'OFS="\t" {print $4,$2,$3,$1,$13}' ${bed_file}_temp.bed | bedtools merge -scores sum -i - > ${bed_file}_coverage.bed
-awk 'OFS="\t" { print $1, $4 / ($3 - $2 + 1)}' ${bed_file}_coverage.bed > ${bed_file}_read_depth.bed
-
-rm ${bed_file}_temp.bed
-
-
+code/make_bed.sh -r $master_ref -p "paired" -b $bam_file -o $bed_file
 
