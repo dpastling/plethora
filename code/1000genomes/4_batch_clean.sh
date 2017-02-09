56500b7ca222f0fde7709c74497a11f50e86a585
David Astling <david.astling@ucdenver.edu>
2017-02-09 14:22:46 -0700

    add a script for cleaning up after the bowtie run

---
+++ code/1000genomes/4_batch_clean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#BSUB -J clean[104,110,112,113,118-121,124-128,130,131]%5
+#BSUB -e logs/clean_%J.log
+#BSUB -o logs/clean_%J.out
+#BSUB -R "select[mem>5] rusage[mem=5] span[hosts=1]"
+#BSUB -q normal
+#BSUB -n 1
+#BSUB -P Sikela
+
+# catch unset variables, non-zero exits in pipes and calls, enable x-trace.
+set -o nounset -o pipefail -o errexit
+
+source code/1000genomes/config.sh
+
+# LSB_JOBINDEX is the job array position
+sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+
+code/1000genomes/3_clean_files.pl --rm-fastq $sample
+
+
