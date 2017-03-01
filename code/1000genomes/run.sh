2a5f00fdd9e92323b4ccc1e0abf5ae70df0abac9
David Astling <david.astling@ucdenver.edu>
2017-03-01 12:29:10 -0700

    update steps in the LSF script run.sh
    - call step 6_clean rather than 4_clean in case the user wants to apply
      a different kind of clean up step
    - currently 6_clean points to 4_clean so nothing effectivly changes

--- code/1000genomes/run.sh
+++ code/1000genomes/run.sh
@@ -8,7 +8,7 @@ bsub -w "done(download[*])" < code/1000genomes/2_trim.sh
 bsub -w "done(trim[*])" < code/1000genomes/3_batch_bowtie.sh
 bsub -w "done(align[*])" < code/1000genomes/4_batch_clean.sh
 bsub -w "done(clean[*])" < code/1000genomes/5_batch_make_bed.sh
-bsub -w "done(coverage[*])" < code/1000genomes/4_batch_clean.sh
+bsub -w "done(coverage[*])" < code/1000genomes/6_batch_clean.sh
 bsub -w "done(clean[*])" < code/1000genomes/7_batch_gc_correction.sh
 
 
