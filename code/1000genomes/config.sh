f85086099255461d60124088f252252a40395c98
Ilea Heft <ilea.eskildsen@ucdenver.edu>
2017-04-28 18:49:29 -0600

    fixes config file ("d" in what should be data/1000Genomes_samples.txt") was missing

--- code/1000genomes/config.sh
+++ code/1000genomes/config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sample_index=ata/1000Genomes_samples.txt
+sample_index=data/1000Genomes_samples.txt
 genome=$HOME/genomes/bowtie2.2.5_indicies/hg38/hg38
 master_ref=data/hg38_duf_full_domains_v2.3.bed
 
